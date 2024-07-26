f554f0d243725c7327b06d1386e4a8cb428e94ea
haraldkangschroeder <haraldkangschroederdevelopment@gmail.com>
2024-07-27 00:11:23 +0200

    feat(layout): add layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,20 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Shelf from './pages/Shelf'
-import Statistics from './pages/Statistics'
-import NavBar from './components/NavBar'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Shelf from "./pages/Shelf";
+import Statistics from "./pages/Statistics";
+import "../app/globals.css";
+import Layout from "@/components/layout/Layout.tsx";
 
 function App() {
-
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shelf" element={<Shelf />} />
         <Route path="/statistics" element={<Statistics />} />
       </Routes>
-    </>
-  )
+    </Layout>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/layout/Layout.tsx
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button.tsx";
+import { LuHome } from "react-icons/lu";
+import { GiBookshelf } from "react-icons/gi";
+import { MdQueryStats } from "react-icons/md";
+
+export default function Layout({ children }) {
+  const navigate = useNavigate();
+
+  const iconSize = 20;
+  return (
+    <div className={"h-[100vh] w-[100vw] bg-background flex"}>
+      <div className={"h-full border border-r-border flex flex-col gap-4 p-2"}>
+        <Button variant="ghost" size="icon" onClick={() => navigate("/")}>
+          <LuHome size={iconSize} />
+        </Button>
+        <Button variant="ghost" size="icon" onClick={() => navigate("/shelf")}>
+          <GiBookshelf size={iconSize} />
+        </Button>
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={() => navigate("/statistics")}
+        >
+          <MdQueryStats size={iconSize} />
+        </Button>
+      </div>
+      <main className={"flex-1 h-full"}>{children}</main>
+    </div>
+  );
+}
