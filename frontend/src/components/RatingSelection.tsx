ba03d791724e73039a4d4884704156251cc872de
leoniemoedl <moedl.leonie@gmail.de>
2024-07-27 15:10:32 +0200

    feat: Add garbage and perfect rating options

--- frontend/src/components/RatingSelection.tsx
+++ frontend/src/components/RatingSelection.tsx
@@ -1,5 +1,6 @@
 import { ComponentProps, useState } from "react";
 import { PiFeatherFill, PiFeatherLight } from "react-icons/pi";
+import { TbFeatherOff } from "react-icons/tb";
 
 export default function RatingSelection() {
   const ratingScale = 7;
@@ -10,14 +11,23 @@ export default function RatingSelection() {
   };
 
   return (
-    <div className="flex">
-      {Array.from({ length: ratingScale }).map((_, index) => (
-        <RatingIcon
-          key={index}
-          isActive={index < rating}
-          onClick={() => selectRating(index + 1)}
-        />
-      ))}
+    <div className="flex gap-5">
+      <TbFeatherOff onClick={() => selectRating(0)} />
+      <div className="flex">
+        {Array.from({ length: ratingScale }).map((_, index) => (
+          <RatingIcon
+            key={index}
+            isActive={index < rating}
+            onClick={() => selectRating(index + 1)}
+          />
+        ))}
+      </div>
+
+      <RatingIcon
+        isActive={ratingScale + 1 == rating}
+        onClick={() => selectRating(ratingScale + 1)}
+        className="text-amber-400"
+      />
     </div>
   );
 }
