674151a78876d12a5ef2870172cfad2b0cb06fb2
leoniemoedl <moedl.leonie@gmail.de>
2024-07-27 22:06:46 +0200

    feat: Introduce chart for distribution of genre and recommended books

--- frontend/src/pages/Statistics.tsx
+++ frontend/src/pages/Statistics.tsx
@@ -1,20 +1,13 @@
-import AverageRatingCategory from "@/modules/book/components/AverageRatingCategory";
+import GenreDistribution from "@/modules/book/components/GenreDistribution";
+import RecommendationDistribution from "@/modules/book/components/RecommendationDistribution";
 
-
-
-const chartData = [
-  { category: "Setting", rating: 7 },
-  { category: "Plot", rating: 3 },
-  { category: "Engagement", rating: 4 },
-  { category: "Characters", rating: 4 },
-  { category: "Story", rating: 5 },
-]
 
 
 export default function Statistics() {
   return (
-    <div className="p-4">
-      <AverageRatingCategory data={chartData}/>
+    <div className="p-4 flex gap-4">
+      <GenreDistribution />
+      <RecommendationDistribution />
     </div>
   )
 }
