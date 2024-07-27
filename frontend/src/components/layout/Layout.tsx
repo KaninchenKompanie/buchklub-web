86a95afa43a3ca16dfbafcc8ded9d69997d38297
haraldkangschroeder <haraldkangschroederdevelopment@gmail.com>
2024-07-27 15:41:46 +0200

    feat(navigation): highlight current page button

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button.tsx";
 import { LuHome } from "react-icons/lu";
 import { GiBookshelf } from "react-icons/gi";
@@ -7,35 +7,42 @@ import { ReactNode } from "react";
 import { PagePaths } from "@/pages/PagePaths";
 
 export default function Layout({ children }: { children: ReactNode }) {
-  const navigate = useNavigate();
-
   const iconSize = 20;
+
   return (
     <div className={"h-[100vh] w-[100vw] bg-background flex"}>
       <div className={"h-full border border-r-border flex flex-col gap-4 p-2"}>
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => navigate(PagePaths.home)}
-        >
-          <LuHome size={iconSize} />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => navigate(PagePaths.shelf)}
-        >
-          <GiBookshelf size={iconSize} />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => navigate(PagePaths.statistics)}
-        >
-          <MdQueryStats size={iconSize} />
-        </Button>
+        <NavigationItem
+          path={PagePaths.home}
+          icon={<LuHome size={iconSize} />}
+        />
+        <NavigationItem
+          path={PagePaths.shelf}
+          icon={<GiBookshelf size={iconSize} />}
+        />
+        <NavigationItem
+          path={PagePaths.statistics}
+          icon={<MdQueryStats size={iconSize} />}
+        />
       </div>
       <main className={"flex-1 h-full"}>{children}</main>
     </div>
+  );
+}
+
+function NavigationItem({ path, icon }: { path: string; icon: ReactNode }) {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const isCurrentPage = location.pathname === path;
+
+  return (
+    <Button
+      variant={isCurrentPage ? "secondary" : "ghost"}
+      size="icon"
+      onClick={() => navigate(path)}
+    >
+      {icon}
+    </Button>
   );
 }
