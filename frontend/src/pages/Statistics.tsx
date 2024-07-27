ebabaa420a3fb53dd3931a8d78819b5d8bd5b096
leoniemoedl <moedl.leonie@gmail.de>
2024-07-27 22:10:32 +0200

    misc: Add example chartData for bar chart

--- frontend/src/pages/Statistics.tsx
+++ frontend/src/pages/Statistics.tsx
@@ -1,11 +1,20 @@
+import AverageRatingCategory from "@/modules/book/components/AverageRatingCategory";
 import GenreDistribution from "@/modules/book/components/GenreDistribution";
 import RecommendationDistribution from "@/modules/book/components/RecommendationDistribution";
 
+const chartData = [
+  { category: "Setting", rating: 7 },
+  { category: "Plot", rating: 3 },
+  { category: "Engagement", rating: 4 },
+  { category: "Characters", rating: 4 },
+  { category: "Story", rating: 5 },
+]
 
 
 export default function Statistics() {
   return (
     <div className="p-4 flex gap-4">
+      <AverageRatingCategory data={chartData} />
       <GenreDistribution />
       <RecommendationDistribution />
     </div>
