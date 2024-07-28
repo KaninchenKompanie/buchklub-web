7bb3f6b8e0690f9c86e3fa2ddb6a9497fb7c98b1
haraldkangschroeder <haraldkangschroederdevelopment@gmail.com>
2024-07-28 19:10:02 +0200

    feat(stats): add most liked book, less liked book and most controversial book radar chart

--- frontend/src/pages/Statistics.tsx
+++ frontend/src/pages/Statistics.tsx
@@ -1,22 +1,22 @@
-import AverageRatingCategory from "@/modules/book/components/AverageRatingCategory";
+import ExtremaBooksStats from "@/modules/book/components/ExtremaBooksStats";
 import GenreDistribution from "@/modules/book/components/GenreDistribution";
 import RecommendationDistribution from "@/modules/book/components/RecommendationDistribution";
-
-const chartData = [
-  { category: "Setting", rating: 7 },
-  { category: "Plot", rating: 3 },
-  { category: "Engagement", rating: 4 },
-  { category: "Characters", rating: 4 },
-  { category: "Story", rating: 5 },
-]
-
+import useBooksStats from "@/modules/book/hooks/useBooksStats";
 
 export default function Statistics() {
+  const { booksStats, isLoading, isError } = useBooksStats();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Failed...</div>;
+
   return (
-    <div className="p-4 flex gap-4">
-      <AverageRatingCategory data={chartData} />
-      <GenreDistribution />
-      <RecommendationDistribution />
+    <div className="p-4 flex flex-col gap-4">
+      {/* <AverageRatingCategory data={chartData} /> */}
+      <ExtremaBooksStats booksStats={booksStats!} />
+      <div className="flex gap-4">
+        <GenreDistribution />
+        <RecommendationDistribution />
+      </div>
     </div>
-  )
+  );
 }
